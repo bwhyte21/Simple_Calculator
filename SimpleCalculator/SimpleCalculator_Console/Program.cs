1338dbd8572e13d415eadcf6ce3380a914f7206a
Bryan Whyte <bwhyte21@outlook.com>
2017-08-03 17:57:13 -0400

    Initial commit with demonstration code that is to be deleted.

---
+++ SimpleCalculator/SimpleCalculator_Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s_someText = "Here is some text";
+            int i_number = 10;
+            double d_number = 10.5;
+            float f_number = 10.6f;
+            bool b_isOrdered = true;
+            string s_input = Console.ReadLine();
+            var v_anyVariable = 10.7;
+
+            Console.WriteLine(s_someText);
+            Console.WriteLine(i_number);
+            Console.WriteLine(d_number);
+            Console.WriteLine(f_number);
+            Console.WriteLine(b_isOrdered);
+            Console.WriteLine(s_input);
+            Console.WriteLine(v_anyVariable);
+
+            return;
+            // ToDo: Clear this and continue from Section 2 Lecture 9
+        }
+    }
+}
