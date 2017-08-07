249a8f63722235b503ae9e76c47912431fab4dbd
Bryan Whyte <bwhyte21@outlook.com>
2017-08-07 15:25:23 -0400

    objects, classes, methods

---
+++ SimpleCalculator/SimpleCalculator_Console/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator_Console
+{
+    public class Person
+    {
+        private int n_age = 26;
+        public int getAge()
+        {
+            return n_age;
+        }
+        public void setAge(int n_newAge)
+        {
+            n_age = n_newAge;
+        }
+    }
+}
--- SimpleCalculator/SimpleCalculator_Console/Program.cs
+++ SimpleCalculator/SimpleCalculator_Console/Program.cs
@@ -6,34 +6,16 @@ namespace SimpleCalculator_Console
     {
         static void Main(string[] args)
         {
-            string s_someText = "Here is Some text";
-            string s_someMoreText = "Here is some text";
+            Person person1 = new Person();
 
-            // using ordinal case for string comparison is faster
-            bool b_isEqual = s_someText.Equals(s_someMoreText, StringComparison.Ordinal);
+            Console.WriteLine(person1.getAge());
+            person1.setAge(27);
+            Console.WriteLine(person1.getAge());
 
-            string s_addedText = s_someText + " " + s_someMoreText + " here is some more text.";
-            string s_formattedString = string.Format("{0} {1} here is some more text.", s_someText, s_someMoreText);
-
-            Console.WriteLine(s_addedText);
-            Console.WriteLine(s_formattedString);
-
-            Console.WriteLine(s_someText.Length);
-            Console.WriteLine(s_someText[8]);
-
-            Console.WriteLine(s_someText.Substring(8, 4));
-
-            Console.WriteLine(s_someText.ToLower());
-            Console.WriteLine(s_someText.ToUpper());
-
-            string s_anotherText = string.Empty;
-            string s_replacedText = s_someText.Replace("text", "string");
-
-            Console.WriteLine(s_replacedText);
 
 
             /* Keep the console window open til user applies input */
-            #if DEBUG
+#if DEBUG
             Console.WriteLine("Press enter to close...");
             Console.ReadLine();
             #endif
