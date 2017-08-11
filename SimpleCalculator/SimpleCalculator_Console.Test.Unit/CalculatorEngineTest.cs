421e658160f85387dcb5af1ae013ee060d9f7783
Bryan Whyte <bwhyte21@outlook.com>
2017-08-11 15:05:44 -0400

    Changes made to code: -Added unit testing to the project

---
+++ SimpleCalculator/SimpleCalculator_Console.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator_Console.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine ce_calculatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int n_number1 = 1;
+            int n_number2 = 2;
+            double d_result = ce_calculatorEngine.Calculate("add", n_number1, n_number2);
+            // Assert is another way to check if the result matches the outcome it's meant to be
+            Assert.AreEqual(3, d_result);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int n_number1 = 1;
+            int n_number2 = 2;
+            double d_result = ce_calculatorEngine.Calculate("+", n_number1, n_number2);
+            Assert.AreEqual(3, d_result);
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator_Console.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator_Console.Test.Unit
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        private readonly InputConverter ic_inputConverter = new InputConverter();
+        [TestMethod]
+        public void ConvertValidStringInputIntoDouble()
+        {
+            string s_inputNo = "5";
+            double d_convertedNo = ic_inputConverter.ConvertInputToNumeric(s_inputNo);
+            Assert.AreEqual(5, d_convertedNo);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailsToConvertInvalidStringInputIntoDouble()
+        {
+            string s_inputNo = "*";
+            double d_convertedNo = ic_inputConverter.ConvertInputToNumeric(s_inputNo);
+            Assert.AreEqual(5, d_convertedNo);
+        }
+    }
+}
